d3f8acb961300b24f06bea090c5e01e37fa8b5d4
Florin Chertes <FlorinChertes@computer.org>
2021-07-18 11:48:42 +0200

    add div by zero

--- CoreCheckExample.cpp
+++ CoreCheckExample.cpp
@@ -1,6 +1,49 @@
 // CoreCheckExample.cpp
 // Add CppCoreCheck package and enable code analysis in build for warnings.
 
+#include <cassert>
+#include <iostream>
+
+int null_deref(int x)
+{
+	int* p = nullptr;
+
+	if (x)
+	{
+		return *p + 13;
+	}
+	else
+	{
+		return 47;
+	}
+}
+
+int div_by_zero(int x)
+{
+	if(x)
+	{
+		return 13 / 0;
+	}
+	else
+	{
+		return 47;
+	}
+}
+
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+void test_utils_024()
+{
+	std::cout << "*** test utils 024 ***" << std::endl;
+
+	//const int ret_val = null_deref(1);
+	//std::cout << "ret val null deref = " << ret_val << std::endl;
+	const int ret_val = div_by_zero(1);
+	std::cout << "ret val div by null = " << ret_val << std::endl;
+
+
+}
+
 int main()
 {
     int arr[10];           // warning C26494
@@ -11,6 +54,8 @@ int main()
         int* q = p + 1;    // warning C26481 (suppressed)
         p = q++;           // warning C26481 (suppressed)
     }
+
+    test_utils_024();
 
     return 0;
 }
